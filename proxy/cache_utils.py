b0bec304a34959c7f526eeafc1484d79e001325f
Ambrose Blay <ab2838@cornell.edu>
2025-11-22 01:58:22 -0500

    implement utilities for cache

---
+++ proxy/cache_utils.py
@@ -0,0 +1,30 @@
+import threading
+from datetime import datetime, timedelta
+
+class TTLCache: 
+    
+    def __init__(self):
+        self.store = {}
+        self.ttl = 180
+        self.lock = threading.Lock()
+        
+    def get(self, key):
+        self.lock.acquire()
+        if key not in self.store:
+            return (None, False)
+
+        value, exp = self.store[key]
+        if datetime.now() > exp:
+            return (None, False)
+        return (value, True)
+    
+    def set(self, key, value):
+        self.lock.acquire()
+        self.store[key] = (value, datetime.now() + timedelta(seconds = 180))
+        
+    def delete(self, key):
+        self.lock.acquire()
+        self.store.pop(key)
+        
+    def size(self):
+        return len(self.store)
