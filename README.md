2f1b74452538f23a5f3f2355261195dbe9f40b9e
Ambrose Blay <ab2838@cornell.edu>
2025-11-22 01:21:39 -0500

    fix bugs in origin_server

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 #This is a project for a proxy that improves the efficiency of access into a data origin using distributed systems principles
+# distributed-proxy
--- origin/origin_server.py
+++ origin/origin_server.py
@@ -1,45 +1,59 @@
 import socket
 import json
 import time
+from datetime import datetime
 
 HOST = "127.0.0.1"
 PORT = 8000
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(5)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen(5)
 
-while True:
+    while True:
 
-    conn, add = s.accept()
+        conn, add = s.accept()
 
-    with conn: 
-        print("Connected by: ", add)
-        data = conn.recv(1024)
-        if not data:
-            break
-        data = data.decode('utf-8').strip()
-        method, url = data.split(" ")
-        resource, key = url.split("/", 1)
-        filepath = "data/" + resource + key + ".json"
-        
-        try: 
-            with open(filepath) as f:
-                fetched_data = json.loads(f.read())
-            res = {
-                "status": "OK",
-                "data": fetched_data
-            }
-            res_string = json.dumps(res)
-            time.sleep(0.1)
-            conn.sendall(res_string.encode('utf-8'))
-        except Exception as e:
-            res = {
-                "status": "NOT_FOUND",
-                "data": "An error occured: {e}"
-            }
-            res_string = json.dumps(res)
-            time.sleep(0.1)
-            conn.sendall(res_string.encode('utf-8'))
+        with conn: 
+            print(f"Connected by: {add} at {datetime.now()}")
+            data = conn.recv(1024)
+            if not data:
+                continue
+            data = data.decode('utf-8').strip()
+            method, url = data.split(" ")
+            
+            if method != "GET":
+                res = {
+                    "status": "WRONG_METHOD",
+                    "data": f"{method} is not currently supported"
+                }
+                res_string = json.dumps(res) + "\n"
+                time.sleep(0.1)
+                conn.sendall(res_string.encode('utf-8'))
+                
+                continue
+                
+            resource, key = url.split("/", 1)
+            filepath = "data/" + resource + key + ".json"
+            print(f"{method} to file at {filepath}")
+            try: 
+                with open(filepath) as f:
+                    fetched_data = json.load(f)
+                res = {
+                    "status": "OK",
+                    "data": fetched_data
+                }
+                res_string = json.dumps(res) + "\n"
+                time.sleep(0.1)
+                conn.sendall(res_string.encode('utf-8'))
+            except Exception as e:
+                res = {
+                    "status": "NOT_FOUND",
+                    "data": f"An error occured: {e}"
+                }
+                print(f"There was an error: {e}" )
+                res_string = json.dumps(res) + "\n"
+                time.sleep(0.1)
+                conn.sendall(res_string.encode('utf-8'))
         
         
