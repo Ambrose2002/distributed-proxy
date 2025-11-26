60a28bb29334509ee15cce92f09b2f58043fb575
Ambrose Blay <ab2838@cornell.edu>
2025-11-25 19:07:06 -0500

    fix path bug in start_cluster

--- scripts/start_cluster.sh
+++ scripts/start_cluster.sh
@@ -15,21 +15,21 @@ rm -f .cluster_pids
 
 # start origin server
 echo "Starting server on $ORIGIN_PORT"
-python origin/origin_server.py &
+python ../origin/origin_server.py &
 echo $! >> .cluster_pids
 sleep 1
 
 # start proxy nodes
 for port in 8001 8002 8003; do 
     echo "Starting proxy on $port"
-    python proxy/proxy_node.py --port $port --origin_port $ORIGIN_PORT --ttl $TTL &
+    python ../proxy/proxy_node.py --port $port --origin_port $ORIGIN_PORT --ttl $TTL &
     echo $! >> .cluster_pids
     sleep 1
 done
 
 # start load balancer
 echo "Starting load balancer on $LB_PORT"
-python load_balancer/load_balancer.py --port $LB_PORT --proxies 127.0.0.1:8001 127.0.0.1:8002 127.0.0.1:8003 --strategy $RB_STRATEGY &
+python ../load_balancer/load_balancer.py --port $LB_PORT --proxies 127.0.0.1:8001 127.0.0.1:8002 127.0.0.1:8003 --strategy $RB_STRATEGY &
 echo $! >> .cluster_pids
 
 echo "Cluster started!"
